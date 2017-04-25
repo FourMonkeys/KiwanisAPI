83e67ce6fcdf3077f18d7a4365a70744d2270849
Matt Livingston <mlivingston2066@gmail.com>
2017-04-25 13:05:43 -0500

    Initial Commit

---
+++ KiwanisAPI.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask import request
+from flask import Response
+
+from bson.json_util import dumps
+
+from services import directory_service
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello World! Welcome to the Kiwanis API'
+
+
+@app.route('/v1/directory', methods=['GET', 'POST'])
+def directory():
+    if request.method == 'GET':
+        cursor = directory_service.list_directory()
+        d = []
+        for c in cursor:
+            d.append(c)
+        return Response(dumps(d), status=200, mimetype="application/json")
+    elif request.method == "POST":
+        member = request.get_json(silent=True)
+
+        directory_service.add_member(member["name"], member["image"], member["phone"],
+                                     member["role"], member["email"])
+        msg = {
+            "Status": "Record Added Successfully"
+        }
+        return Response(dumps(msg), status=200, mimetype="application/json")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
---
+++ services/directory_service.py
@@ -0,0 +1,36 @@
+import pymongo
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client.Kiwanis
+
+
+def list_directory():
+    collection = db.Directory
+    return collection.find()
+
+
+def add_member(name, image, phone, role, email):
+    collection = db.Directory
+    member = {
+        "Name": name,
+        "Image": image,
+        "Phone": phone,
+        "Role": role,
+        "Email": email
+    }
+    message = {}
+    code = 0
+    try:
+        collection.insert_one(member)
+        message = {
+            "Status": "Ok"
+        }
+        code = 1
+    except Exception as e:
+        message = {
+            "Status": "An Error Occurred: " + str(e)
+        }
+        code = 2
+
+    return code, message
